e2e089ea377ff53c837d5fb17fa99f1e88579723
Vinod Godti <venkat.godti3@gmail.com>
2024-09-14 23:25:17 +0530

    Lift movement with peristent postion

--- src/js/main.js
+++ src/js/main.js
@@ -66,10 +66,22 @@ Queue concept to be implemented
 // l3->f2;
 
 //ctl -> up f4
-// check for lift are idle and nearest
+// check for lift are idle and nearest and only 2lifts allowed in one
+// floor[one is for UP and other one is for DOWN]
 // pick that
+/**
+ * Someting creative i need to comeup with 1 Floor and 1 Lift
+ */
+const STATUS = {
+  IDLE: "IDLE",
+  MOVING: "MOVING",
+};
+const DIRECTION = {
+  UP: "UP",
+  DOWN: "DOWN",
+};
 const buildingWrapper = document.getElementById("lifts-wrapper");
-const submitBtn = document.getElementById("submit-btn");
+const submitForm = document.getElementById("submit-form");
 const btnClassList = [
   "text-white",
   "bg-gradient-to-br",
@@ -127,7 +139,13 @@ function renderFloor(floorObj, lifts) {
     "h-20",
     "col-span-10"
   );
-  if (floorObj.floorNumber === 1) floor.appendChild(renderLifts(lifts));
+  const liftWrapper = document.createElement("div");
+  liftWrapper.id = floorObj.floorName;
+  liftWrapper.classList.add("flex", "gap-2", "pb-2", "-m-1");
+  //If only one floor no lifts required
+  floorObj.floorNumber === 1
+    ? floor.appendChild(renderLifts(lifts, liftWrapper))
+    : floor.appendChild(liftWrapper);
   return floor;
 }
 function renderFloorName(floorName) {
@@ -140,43 +158,110 @@ function renderFloorName(floorName) {
 function renderBuildingAndLifts(floors, lifts) {
   buildingWrapper.innerHTML = null;
   const building = document.createElement("div");
-  building.classList.add("grid", "grid-cols-12", "gap-4");
-  floors.forEach((floor, i) => {
+  building.classList.add("grid", "grid-cols-12", "gap-4", "mt-4");
+  floors.forEach((floor) => {
     building.appendChild(renderLiftControl(floor, floors.length));
     building.appendChild(renderFloor(floor, lifts));
     building.appendChild(renderFloorName(floor.floorName));
   });
-  buildingWrapper.append(building);
+  buildingWrapper.appendChild(building);
+  const renderedLifts = liftController.getLifts();
+  //To make lifts persistent in same position
+  renderedLifts.forEach((lift) => {
+    const liftElement = document.getElementById(lift.liftNo);
+    const liftRect = liftElement.getBoundingClientRect();
+    liftElement.style.left = `${liftRect.left}px`;
+  });
+  renderedLifts.forEach((lift) => {
+    const liftElement = document.getElementById(lift.liftNo);
+    liftElement.style.position = `absolute`;
+  });
 }
 
-function renderLifts(lifts) {
-  const liftWrapper = document.createElement("div");
-  liftWrapper.classList.add("flex", "flex-row", "gap-2", "pb-2", "-m-1");
-  lifts.forEach((lift) => {
-    console.log(lift);
+function renderLifts(lifts, liftWrapper) {
+  lifts.forEach((lift, index) => {
     const liftElement = document.createElement("div");
-    liftElement.classList.add("bg-blue-500", "w-16", "h-20", "rounded-md");
+    liftElement.id = lift.liftNo;
+    const leftDoor = document.createElement("div");
+    leftDoor.classList.add("door-left");
+    const rightDoor = document.createElement("div");
+    rightDoor.classList.add("door-right");
+    liftElement.appendChild(leftDoor);
+    liftElement.appendChild(rightDoor);
+    liftElement.classList.add(
+      "bg-gray-500",
+      "w-16",
+      "h-20",
+      "rounded-sm",
+      "border-2",
+      "border-gray-500",
+      "relative",
+      "lift"
+    );
     liftWrapper.appendChild(liftElement);
   });
   return liftWrapper;
 }
 
-submitBtn.addEventListener("click", function () {
-  const noOfFloors = 10;
-  const noOfLifts = 15;
+submitForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const noOfFloorsElement = document.getElementById("no-of-floors");
+  const noOfLiftsElement = document.getElementById("no-of-lifts");
+  const noOfFloors = noOfFloorsElement.value;
+  const noOfLifts = noOfLiftsElement.value;
   liftController.renderBuilding(noOfFloors, noOfLifts);
 });
 
-function renderLiftMovement(fromFloor, toFloor, direction, liftNo) {}
+function renderLiftMovement({
+  fromFloor,
+  toFloor,
+  fromFloorName,
+  toFloorName,
+  direction,
+  liftNo,
+  openLiftDoors,
+}) {
+  const noOfFloors = toFloor - fromFloor;
+  const timeRequiredToReach = noOfFloors * 2; // Each floor crossing time 2 secs
+  console.log(
+    timeRequiredToReach,
+    fromFloor,
+    toFloor,
+    fromFloorName,
+    toFloorName
+  );
+  const source = document.getElementById(fromFloorName);
+  const destination = document.getElementById(toFloorName);
+  const sourceRect = source.getBoundingClientRect();
+  const destinationRect = destination.getBoundingClientRect();
 
-const STATUS = {
-  IDLE: "IDLE",
-  MOVING: "MOVING",
-};
-const DIRECTION = {
-  UP: "UP",
-  DOWN: "DOWN",
-};
+  const movingLift = document.getElementById(liftNo);
+  const movingLiftRect = movingLift.getBoundingClientRect();
+  movingLift.style.position = "absolute";
+  movingLift.style.top = `${sourceRect.top + window.scrollY}px`;
+  movingLift.style.left = `${movingLiftRect.left + window.scrollX}px`;
+  movingLift.classList.add("moving");
+  const deltaY = destinationRect.top - sourceRect.top;
+  const deltaX = destinationRect.left - sourceRect.left;
+  movingLift.addEventListener("transitionend", (e) => {
+    // Optionally, you can do something like swap content or hide the source
+    movingLift.classList.remove("moving");
+    movingLift.style = "";
+    movingLift.style.position = "absolute";
+    movingLift.style.left = `${movingLiftRect.left}px`;
+    destination.appendChild(movingLift);
+    if (e.propertyName === "transform") {
+      openLiftDoors(toFloor);
+    }
+  });
+  // Trigger the animation using CSS transform
+  requestAnimationFrame(() => {
+    //movingLift.style.left = leftPosition;
+    movingLift.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    movingLift.style.transitionDuration = `${Math.abs(timeRequiredToReach)}s`;
+  });
+}
+
 class Lift {
   constructor(floor, liftNo) {
     this.floor = floor;
@@ -187,21 +272,52 @@ class Lift {
   move(direction, floor) {
     const fromFloor = this.floor;
     const toFloor = floor;
-    this.status = STATUS.MOVING;
     this.direction = direction;
-    this.renderMove(fromFloor, toFloor, direction, this.liftNo);
-    this.floor = floor;
-    this.open();
-    this.close();
-    this.status = STATUS.IDLE;
+    this.status = STATUS.MOVING;
+    if (fromFloor !== toFloor) {
+      this.renderMove({
+        fromFloor,
+        toFloor,
+        direction,
+        liftNo: this.liftNo,
+        openLiftDoors: this.open.bind(this),
+      });
+    } else {
+      this.open(floor);
+    }
   }
-  open() {
-    console.log("Opening the door");
+  open(floor) {
+    console.log("Opening the door", floor);
+    this.floor = floor;
+    setTimeout(() => {
+      document.getElementById(this.liftNo).classList.add("slide");
+    }, 0);
+    this.close();
   }
   close() {
-    console.log("Closing the door");
+    const lift = document.getElementById(this.liftNo);
+    setTimeout(() => {
+      lift.classList.remove("slide");
+    }, 2500);
+    lift.addEventListener("transitionend", (e) => {
+      if (e.propertyName === "left") {
+        this.status = STATUS.IDLE;
+      }
+    });
   }
-  renderMove(fromFloor, toFloor, direction, liftNo) {}
+  renderMove({ fromFloor, toFloor, direction, liftNo, openLiftDoors }) {
+    const fromFloorName = `FLOOR-${fromFloor}`;
+    const toFloorName = `FLOOR-${toFloor}`;
+    renderLiftMovement({
+      fromFloor,
+      toFloor,
+      fromFloorName,
+      toFloorName,
+      direction,
+      liftNo,
+      openLiftDoors,
+    });
+  }
   getLiftPosition() {
     return this.floor;
   }
@@ -213,7 +329,6 @@ class Floor {
     this.floorName = `FLOOR-${floorNumber}`;
   }
 }
-
 class LiftController {
   constructor() {
     this.totalFloors = 0;
@@ -221,9 +336,14 @@ class LiftController {
     this.lifts = [];
     this.floors = [];
   }
+  getLifts() {
+    return this.lifts;
+  }
   renderBuilding(noOfFloors, noOfLifts) {
     this.totalFloors = noOfFloors;
     this.totalLifts = noOfLifts;
+    this.lifts = [];
+    this.floors = [];
     for (let i = 0; i < this.totalLifts; i++) {
       let lift = new Lift(1, `lift-${i + 1}`);
       this.lifts.push(lift);
@@ -234,11 +354,18 @@ class LiftController {
     }
     renderBuildingAndLifts(this.floors, this.lifts);
   }
-  liftRequest(floorNumber, direction) {
-    this.lifts.map((lift) => {
-      lift.move(direction, floorNumber);
+  getAvailableLift(floorNumber, direction) {
+    return this.lifts.find((lift, i) => {
+      if (lift.status === STATUS.IDLE && lift.direction === direction) {
+        return lift;
+      }
     });
+  }
+  liftRequest(floorNumber, direction) {
+    const lift = this.getAvailableLift(floorNumber, direction);
+    lift?.move(direction, floorNumber);
   }
 }
 
 const liftController = new LiftController();
+//liftController.getLifts();
