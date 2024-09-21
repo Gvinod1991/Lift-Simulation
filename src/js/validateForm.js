ffa6ad04e92036e912a2d4dd15151f553a5badd0
Vinod Godti <venkat.godti3@gmail.com>
2024-09-21 18:22:25 +0530

    Validation issue for no of lifts issue fix

--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -1,10 +1,10 @@
 const validateForm = (formValues) => {
   const errors = {};
-  if (!formValues.noOfLifts || formValues.noOfLifts <= 1) {
-    errors["noOfLifts"] = "No of Lifts should be above 1";
-  }
   if (!formValues.noOfFloors || formValues.noOfFloors <= 1) {
     errors["noOfFloors"] = "No of Floors should be above 1";
+  }
+  if (!formValues.noOfLifts || formValues.noOfLifts < 1) {
+    errors["noOfLifts"] = "No of Lifts should be 1 or above";
   }
   const isFormValid = Object.keys(errors).length === 0;
   return { isFormValid, errors };
